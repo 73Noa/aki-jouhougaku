769d828bde0c187cb6003d24afcb53d69231baef
73Noa <167832828+73Noa@users.noreply.github.com>
2024-10-22 10:22:51 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -2,8 +2,3 @@ var who65='world';
 function sayhello65(){alert('Hello,'+who65+'!');}
 function taro65(){who65='太郎';}
 function hanako65(){who65='花子';}
-
-var who66='world';
-function sayhello66(){alert('Hello,'+who66+'!');}
-function taro66(){alert('!');}
-function hanako66(){alert('花子が来た！');}
