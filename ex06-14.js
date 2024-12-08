d765d5351ba1e07933af3052be13860d33ed7df1
73Noa <167832828+73Noa@users.noreply.github.com>
2024-12-08 18:12:35 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,2 +1,3 @@
-function imgchange3(){document.getElementById('logo').src='./img/aikon.png'}
-function imgchange4(){document.getElementById('logo').src='./img/logo.png'}
+function imgchange3(){document.getElementById('logo').src='./img/logo.png'}
+function imgchange4(){document.getElementById('logo').src='./img/aikon.png';
+                     setTimeout("imgchange3()",3000);}
