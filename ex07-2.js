5340263a845fdc53e2208e5a130f18661f45b339
73Noa <167832828+73Noa@users.noreply.github.com>
2024-12-13 00:34:52 +0900

    Update ex07-2.js

--- ex07-2.js
+++ ex07-2.js
@@ -1,3 +1,3 @@
-var x=document.getElementById('input1').value
-var y=document.getElementById('input2').value
-function add(){alert(Number('x')+Number('y'));}
+function add(){var x=document.getElementById('input1').value;
+               var y=document.getElementById('input2').value;
+               alert(Number('x')+Number('y'));}
