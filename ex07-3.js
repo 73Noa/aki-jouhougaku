46f76726eca4d4a9c27c497a88836e786af797ee
73Noa <167832828+73Noa@users.noreply.github.com>
2024-12-23 02:52:13 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,3 +1,4 @@
-function body(){var height = Number(document.getElementById("input8").value);
-                var weight = Number(document.getElementById("input9").value);
-                alert(weight / ((height / 100) * (height / 100));}
+function calculateBMI(){var height = Number(document.getElementById("input8").value);
+                        var weight = Number(document.getElementById("input9").value);
+                       var bmi = weight / ((height / 100) **2);
+                       alert(bmi.toFixed(2));}
