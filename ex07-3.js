ea7ac886d33b2cf9220ad04c9de975a7aa13a841
73Noa <167832828+73Noa@users.noreply.github.com>
2024-12-23 02:45:21 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,4 +1,3 @@
-function body(){
-  var height = Number(document.getElementById("input8").value);
-  var weight = Number(document.getElementById("input9").value);
-  alert(weight / ((height / 100) * (height / 100));}
+function body(){var height = Number(document.getElementById("input8").value);
+                var weight = Number(document.getElementById("input9").value);
+                alert(weight / ((height / 100) * (height / 100));}
