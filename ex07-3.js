ab6f62be31215c20dbfcb9e3c95a8c01abce4efc
73Noa <167832828+73Noa@users.noreply.github.com>
2024-12-23 02:34:23 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,4 +1,4 @@
 function body(){
   var height = Number(document.getElementById("input8").value);
   var weight = Number(document.getElementById("input9").value);
-  alert(weight / ((height / 100) ** 2));}
+  alert(weight / ((height / 100) * (height / 100));}
