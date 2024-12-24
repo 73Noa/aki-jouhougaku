c83bf7d36953a9f42fd13aa19ac15cc145483dab
73Noa <167832828+73Noa@users.noreply.github.com>
2024-12-24 09:31:13 +0900

    Update ex07-7.js

--- ex07-7.js
+++ ex07-7.js
@@ -1,7 +1,7 @@
-function nazo77(){
-  if(document.getElementById('answer77').value == '夢'){
-    alert('あたり！');
-  }else{
-    alert('はずれ！');
-  }
+function nazo77() {
+    if (document.getElementById("answer77").value == "夢") {
+        alert('あたり！');
+    } else {
+        alert('はずれ');
+    }
 }
