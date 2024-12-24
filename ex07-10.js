ecfc22681060a62e1c471e10d4a760882e7c5c56
73Noa <167832828+73Noa@users.noreply.github.com>
2024-12-24 10:42:53 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,4 +1,13 @@
 function calculateBMI2(){var height = Number(document.getElementById("input10").value);
-                        var weight = Number(document.getElementById("input11").value);
-                       var bmi = weight / ((height / 100) **2);
-                       alert(bmi.toFixed(3));}
+                         var weight = Number(document.getElementById("input11").value);
+                         var bmi = weight / ((height / 100) **2);
+                         alert(bmi.toFixed(3));}
+function BMI10(){
+    if(document.getElementById("answer99").value == "夢"){
+        alert('あたり！');
+    }else if(document.getElementById("answer99").value == "ゆめ"){
+        alert('あたり！');
+      }else{
+        alert('はずれ');
+    }
+}
