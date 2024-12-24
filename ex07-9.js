ae861b3f1a2189a3e5bdad32a144bea7121cb003
73Noa <167832828+73Noa@users.noreply.github.com>
2024-12-24 09:42:27 +0900

    Create ex07-9.js

---
+++ ex07-9.js
@@ -0,0 +1,11 @@
+function nazo99(){
+    if(document.getElementById("answer99").value == "夢"){
+        alert('あたり！');
+    }else{
+        alert('はずれ');
+    }else{
+      if(document.getElementById("answer99").value == "ゆめ"){
+        alert('あたり！');
+      }
+    }
+}
